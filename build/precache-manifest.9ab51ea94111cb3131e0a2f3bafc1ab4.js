d0c3e35fcc17831841b974097f5ed8163f620367
akshay0077 <kherakshay007@gmail.com>
2021-11-06 17:47:36 +0530

    production-mode

---
+++ build/precache-manifest.9ab51ea94111cb3131e0a2f3bafc1ab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678b0e7c50a30fb68bf3d827162210a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "223926e9e38cdc6aac84",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "9d8f331d399987896e35",
+    "url": "/static/js/2.59908bb5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.59908bb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223926e9e38cdc6aac84",
+    "url": "/static/js/main.cd2f893a.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  }
+]);
